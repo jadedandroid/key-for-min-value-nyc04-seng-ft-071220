30fb6df851317fcd3420a10171ae0d38f380a160
jade decordova <lacroixjaded@gmail.com>
2020-07-29 13:45:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  name_hash.each_with_object([]) {|key, value, a|
-  a << value
+# def key_for_min_value(name_hash)
+#   name_hash.each_with_object([]) {|key, value, a|
+#   a << value
    
      
      
-   }
+#   }
     
-  }
+#   }
 
 end
